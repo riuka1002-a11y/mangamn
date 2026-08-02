585830c7f6d9988d9b6c205166ea68eef9f9cbf3
riuka1002-a11y <riuka1002@gmail.com>
2026-08-02 12:44:11 +0800

    Fix Supabase configuration

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 window.APP_CONFIG = {
-  SUPABASE_URL:https://dqxnwgryjxjvrhsvyfco.supabase.co,
-  SUPABASE_ANON_KEY: "sb_publishable_E3w0NmoaHBX_dFaTCmpg4A_X3rgSxNn
+  SUPABASE_URL: "https://dqxnwgryjxjvrhsvyfco.supabase.co",
+  SUPABASE_ANON_KEY: "sb_publishable_E3w0NmoaHBX_dFaTCmpg4A_X3rgSxNn"
 };
